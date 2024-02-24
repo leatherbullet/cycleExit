cf9a06960bfb7fdbdbd3c8e13c230b1e3c902e20
79612 <79612@DESKTOP-O1P5TC5>
2024-02-24 14:53:08 +0300

    Добавьте файлы проекта.

---
+++ выход/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace выход
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitWord = "exit";
+
+            Console.WriteLine("для начала работы цикла нажмите ENTER");
+            string userInput = Console.ReadLine();
+
+            while (exitWord != userInput)
+            {
+                Console.WriteLine("для выхода введите " + exitWord);
+                string exitUserInput = Console.ReadLine();
+
+                if (exitUserInput == exitWord)
+                {
+                    exitWord = userInput;
+                    Console.WriteLine("вы вышли из цикла");
+                }
+
+            }
+        }
+    }
+}
